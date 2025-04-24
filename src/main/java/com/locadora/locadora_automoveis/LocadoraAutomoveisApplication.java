b110cc9ec5afe4d1f0153bcd3db7491341c9c6ab
LeoMallet04 <leonardoms.2010@hotmail.com>
2025-04-24 20:13:04 -0300

    first commit

---
+++ src/main/java/com/locadora/locadora_automoveis/LocadoraAutomoveisApplication.java
@@ -0,0 +1,13 @@
+package com.locadora.locadora_automoveis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocadoraAutomoveisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocadoraAutomoveisApplication.class, args);
+	}
+
+}
