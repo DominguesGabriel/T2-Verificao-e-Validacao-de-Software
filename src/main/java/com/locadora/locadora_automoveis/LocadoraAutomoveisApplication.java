3a8dc485239c756e3651b113f78f49a1fc6ddcb4
Felipe Valiati <felipe.valiati@edu.pucrs.br>
2025-04-29 20:20:54 -0300

    feat endpoint listaautomoveis

--- src/main/java/com/locadora/locadora_automoveis/LocadoraAutomoveisApplication.java
+++ src/main/java/com/locadora/locadora_automoveis/LocadoraAutomoveisApplication.java
@@ -1,13 +1,19 @@
 package com.locadora.locadora_automoveis;
 
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import com.locadora.locadora_automoveis.Models.Automovel;
+import com.locadora.locadora_automoveis.Services.Cadastro.CadastroAutomovel;
 import com.locadora.locadora_automoveis.Services.InicializacaoDados.InicializaAutomoveis;
 import com.locadora.locadora_automoveis.Services.InicializacaoDados.InicializaClientes;
 
 @SpringBootApplication
 public class LocadoraAutomoveisApplication {
+	CadastroAutomovel cadastroAutomovel = CadastroAutomovel.getInstance();
 
 	public static void main(String[] args) {
 		InicializaAutomoveis.inicializar();
@@ -16,4 +22,8 @@ public class LocadoraAutomoveisApplication {
 		SpringApplication.run(LocadoraAutomoveisApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public List<Automovel> listaAutomoveis() {
+		return cadastroAutomovel.listarAutomoveis();
+	}
 }
