a6fc13ff81e7f4df60c63d253247737fe4133927
Felipe Valiati <felipe.valiati@edu.pucrs.br>
2025-04-29 20:09:08 -0300

    inicializacao dos dados

--- src/main/java/com/locadora/locadora_automoveis/LocadoraAutomoveisApplication.java
+++ src/main/java/com/locadora/locadora_automoveis/LocadoraAutomoveisApplication.java
@@ -3,10 +3,16 @@ package com.locadora.locadora_automoveis;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.locadora.locadora_automoveis.Services.InicializacaoDados.InicializaAutomoveis;
+import com.locadora.locadora_automoveis.Services.InicializacaoDados.InicializaClientes;
+
 @SpringBootApplication
 public class LocadoraAutomoveisApplication {
 
 	public static void main(String[] args) {
+		InicializaAutomoveis.inicializar();
+		InicializaClientes.inicializar();
+
 		SpringApplication.run(LocadoraAutomoveisApplication.class, args);
 	}
 
---
+++ src/main/java/com/locadora/locadora_automoveis/Services/InicializacaoDados/InicializaAutomoveis.java
@@ -0,0 +1,21 @@
+package com.locadora.locadora_automoveis.Services.InicializacaoDados;
+
+import com.locadora.locadora_automoveis.Services.Cadastro.CadastroAutomovel;
+
+public class InicializaAutomoveis {
+
+    public static void inicializar() {
+        CadastroAutomovel cadastroAutomovel = CadastroAutomovel.getInstance();
+
+        cadastroAutomovel.cadastrarAutomovel("ABC1234", 2020, 150.0);
+        cadastroAutomovel.cadastrarAutomovel("DEF5678", 2019, 140.0);
+        cadastroAutomovel.cadastrarAutomovel("GHI9012", 2021, 160.0);
+        cadastroAutomovel.cadastrarAutomovel("JKL3456", 2018, 130.0);
+        cadastroAutomovel.cadastrarAutomovel("MNO7890", 2022, 170.0);
+        cadastroAutomovel.cadastrarAutomovel("PQR1234", 2017, 120.0);
+        cadastroAutomovel.cadastrarAutomovel("STU5678", 2020, 155.0);
+        cadastroAutomovel.cadastrarAutomovel("VWX9012", 2019, 145.0);
+        cadastroAutomovel.cadastrarAutomovel("YZA3456", 2021, 165.0);
+        cadastroAutomovel.cadastrarAutomovel("BCD7890", 2023, 180.0);
+    }
+}
