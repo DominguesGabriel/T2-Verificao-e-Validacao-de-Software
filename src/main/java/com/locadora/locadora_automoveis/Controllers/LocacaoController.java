31d24a0a38292dd50791789978d7a426b5fe93d3
Felipe Valiati <felipe.cvaliati@gmail.com>
2025-05-05 16:34:55 -0300

    fix getAllLocacoes method

--- src/main/java/com/locadora/locadora_automoveis/Controllers/LocacaoController.java
+++ src/main/java/com/locadora/locadora_automoveis/Controllers/LocacaoController.java
@@ -1,23 +1,19 @@
 package com.locadora.locadora_automoveis.Controllers;
 
-import com.locadora.locadora_automoveis.Services.Cadastro.CadastroCliente;
+import com.locadora.locadora_automoveis.Models.Locacao;
 import com.locadora.locadora_automoveis.Services.Cadastro.CadastroLocacao;
-import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/acmerent")
 public class LocacaoController {
-
-    @Autowired
-    private CadastroLocacao cadastroLocacao;
-
     @GetMapping("/listalocacoes")
-    private void getAllLocacoes() {
-        //EXEMPLO
-        cadastroLocacao.listarLocacoes();
+    private List<Locacao> getAllLocacoes() {
+        return CadastroLocacao.getInstance().listarLocacoes();
     }
 }
