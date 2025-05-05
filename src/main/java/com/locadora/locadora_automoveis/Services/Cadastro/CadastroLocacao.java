75c47b314baddabc4133171a360fae186b589e1d
Matheus Seibt <matheus.seibt@gmail.com>
2025-05-05 09:58:45 -0300

    Arrumando a lógica da Service

--- src/main/java/com/locadora/locadora_automoveis/Services/Cadastro/CadastroLocacao.java
+++ src/main/java/com/locadora/locadora_automoveis/Services/Cadastro/CadastroLocacao.java
@@ -1,9 +1,9 @@
 package com.locadora.locadora_automoveis.Services.Cadastro;
 
 import com.locadora.locadora_automoveis.Models.Locacao;
-import org.springframework.stereotype.Service;
-import com.locadora.locadora_automoveis.Models.Automovel;
 import com.locadora.locadora_automoveis.Models.Cliente;
+import com.locadora.locadora_automoveis.Models.Automovel;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,22 +12,18 @@ import java.util.List;
 public class CadastroLocacao {
     private List<Locacao> locacoes;
     private int nextId;
-    private static CadastroLocacao instance;
 
-    private CadastroLocacao() {
+    public CadastroLocacao() {
         this.locacoes = new ArrayList<>();
         this.nextId = 1; // ID inicial
     }
 
-    // Singleton
-    public static CadastroLocacao getInstance() {
-        if (instance == null) {
-            instance = new CadastroLocacao();
-        }
-        return instance;
-    }
-
     public Locacao cadastrarLocacao(int dias, Cliente cliente, Automovel automovel) {
+        if (!automovel.isDisponivel()) {
+            throw new IllegalStateException("Automóvel não está disponível para locação");
+        }
+
+        automovel.setDisponivel(false);
         Locacao locacao = new Locacao(nextId++, dias, cliente, automovel);
         locacoes.add(locacao);
         return locacao;
@@ -40,4 +36,15 @@ public class CadastroLocacao {
     public boolean apagarLocacao(int id) {
         return locacoes.removeIf(locacao -> locacao.getId() == id);
     }
+
+    public boolean finalizarLocacao(int id) {
+        for (Locacao locacao : locacoes) {
+            if (locacao.getId() == id) {
+                locacao.getAutomovel().setDisponivel(true);
+                locacoes.remove(locacao);
+                return true;
+            }
+        }
+        return false;
     }
+}
