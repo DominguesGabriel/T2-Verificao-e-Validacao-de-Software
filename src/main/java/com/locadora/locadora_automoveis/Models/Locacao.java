8e717e811d8de2aa936477872451b05f3d5e7424
Felipe Valiati <felipe.cvaliati@gmail.com>
2025-05-06 14:09:25 -0300

    fix getDias breaking if no endDate was defined

--- src/main/java/com/locadora/locadora_automoveis/Models/Locacao.java
+++ src/main/java/com/locadora/locadora_automoveis/Models/Locacao.java
@@ -15,6 +15,10 @@ public class Locacao {
     private Automovel automovel;
 
     public int getDias() {
+        if (endDate == null) {
+            return -1; // Se a locação não foi finalizada, retorna 0 dias
+        }
+
         long diff = endDate.getTime() - startData.getTime();
         return (int) (diff / (1000 * 60 * 60 * 24));
     }
