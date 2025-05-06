b6cdfeae253bf0f30871f3b1223e611cbdf2169f
Felipe Valiati <felipe.cvaliati@gmail.com>
2025-05-06 14:52:09 -0300

    feat InicializaLocacao

---
+++ src/main/java/com/locadora/locadora_automoveis/Services/InicializacaoDados/InicializaLocacao.java
@@ -0,0 +1,41 @@
+package com.locadora.locadora_automoveis.Services.InicializacaoDados;
+
+import java.time.Instant;
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import com.locadora.locadora_automoveis.Services.Cadastro.CadastroAutomovel;
+import com.locadora.locadora_automoveis.Services.Cadastro.CadastroCliente;
+import com.locadora.locadora_automoveis.Services.Cadastro.CadastroLocacao;
+
+@Component
+public class InicializaLocacao implements CommandLineRunner {
+    @Autowired
+    private CadastroLocacao cadastroLocacao;
+    
+    @Autowired
+    private CadastroAutomovel cadastroAutomovel;
+
+    @Autowired
+    private CadastroCliente cadastroCliente;
+
+    @Override
+    public void run(String... args) throws Exception {
+        cadastroLocacao.cadastrarLocacao(
+            Date.from(Instant.ofEpochSecond(352398573)),
+            cadastroCliente.getCliente(1),
+            cadastroAutomovel.getAutomovel(1)
+        ).setEndDate(Date.from(Instant.ofEpochSecond(352498573)));
+
+        cadastroLocacao.cadastrarLocacao(
+            Date.from(Instant.ofEpochSecond(23423443)),
+            cadastroCliente.getCliente(2),
+            cadastroAutomovel.getAutomovel(2)
+        ).setEndDate(Date.from(Instant.ofEpochSecond(33424443)));
+
+        System.out.println("cadastro de locação inicializado com sucesso!");
+    }
+}
