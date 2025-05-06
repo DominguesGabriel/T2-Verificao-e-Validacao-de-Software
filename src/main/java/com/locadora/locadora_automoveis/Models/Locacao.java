989a16340e5085aa32d324c30327f76500b65f1a
Felipe Valiati <felipe.valiati@edu.pucrs.br>
2025-05-05 21:32:25 -0300

    feat locacao now stores start and end date instead of day count

--- src/main/java/com/locadora/locadora_automoveis/Models/Locacao.java
+++ src/main/java/com/locadora/locadora_automoveis/Models/Locacao.java
@@ -1,5 +1,7 @@
 package com.locadora.locadora_automoveis.Models;
 
+import java.util.Date;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -7,7 +9,13 @@ import lombok.Data;
 @AllArgsConstructor
 public class Locacao {
     private int id;
-    private int dias;
+    private Date startData;
+    private Date endDate;
     private Cliente cliente;
     private Automovel automovel;
+
+    public int getDias() {
+        long diff = endDate.getTime() - startData.getTime();
+        return (int) (diff / (1000 * 60 * 60 * 24));
+    }
 }
