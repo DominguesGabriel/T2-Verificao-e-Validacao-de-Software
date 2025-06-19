54c36bd558b863c88762fb405c89c90cf2c030dd
Luiz <kubaszewski001@hotmail.com>
2025-06-19 18:30:31 -0300

    O teste para calcular o valor estava recebendo um automovel null, pois o test não estava inicinado o mock, adicionei o mockito Extension para cuidar disso.

--- src/test/java/com/locadora/locadora_automoveis/Models/LocacaoTest.java
+++ src/test/java/com/locadora/locadora_automoveis/Models/LocacaoTest.java
@@ -6,8 +6,12 @@ import java.time.LocalDate;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.exceptions.base.MockitoException;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 public class LocacaoTest {
     private Locacao locacao;
 
