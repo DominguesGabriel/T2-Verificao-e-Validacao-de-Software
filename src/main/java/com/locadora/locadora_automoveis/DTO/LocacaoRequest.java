7663881829994fe105e6ce1189125c6f457cce93
LeoMallet04 <leonardoms.2010@hotmail.com>
2025-06-19 13:33:07 -0300

    Ajustes de Warnings

--- src/main/java/com/locadora/locadora_automoveis/DTO/LocacaoRequest.java
+++ src/main/java/com/locadora/locadora_automoveis/DTO/LocacaoRequest.java
@@ -11,7 +11,6 @@ import lombok.AllArgsConstructor;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
-import java.util.Date;
 
 @AllArgsConstructor
 public class LocacaoRequest {
