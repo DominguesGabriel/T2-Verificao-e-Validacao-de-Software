b5f3112a4b2e78e91e26784c6dda09e992132821
LeoMallet04 <leonardoms.2010@hotmail.com>
2025-06-22 14:13:52 -0300

    Criação do teste de sistema para simular a Jornada do Usuário

--- src/test/java/com/locadora/locadora_automoveis/Controllers/ACMERentControllerIntegrationTest.java
+++ src/test/java/com/locadora/locadora_automoveis/Controllers/ACMERentControllerIntegrationTest.java
@@ -16,7 +16,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class ACMERentControllerIT {
+public class ACMERentControllerIntegrationTest {
 
     @Autowired
     private MockMvc mockMvc;
